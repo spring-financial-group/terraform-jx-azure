c9daac006c3dfae0a2f52a88d3c156f27010da29
mentlak <mentlak@outlook.com>
2025-03-25 14:14:51 +0000

    fix: change to delete for system assigned

--- cluster/terraform-jx-cluster-aks/cluster/main.tf
+++ cluster/terraform-jx-cluster-aks/cluster/main.tf
@@ -149,7 +149,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "kubesystem" {
   count                 = var.kubesystem_node_size == "" ? 0 : 1
   name                  = "kubesystem"
   priority              = "Regular"
-  eviction_policy       = "Deallocate"
+  eviction_policy       = "Delete"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
   vm_size               = var.kubesystem_node_size
   vnet_subnet_id        = var.vnet_subnet_id
@@ -160,6 +160,10 @@ resource "azurerm_kubernetes_cluster_node_pool" "kubesystem" {
   auto_scaling_enabled  = var.max_kubesystem_node_count == null ? false : true
   mode                  = "System"
   node_taints           = ["CriticalAddonsOnly=true:NoSchedule"]
+
+  node_labels = {
+    "nodepool" = "kubesystem"
+  }
 
   lifecycle { ignore_changes = [node_taints, node_count, node_labels] }
 } 
