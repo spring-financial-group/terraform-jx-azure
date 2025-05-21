cd777343db6be7657bbb69f321091f97fee36a2c
Tony Snowden <tony.snowden@mqube.com>
2025-05-21 10:57:06 +0100

    chore: tfsec suggestions

--- cluster/terraform-jx-cluster-aks/cluster/main.tf
+++ cluster/terraform-jx-cluster-aks/cluster/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   azure_active_directory_role_based_access_control {
-    azure_rbac_enabled = false
+    azure_rbac_enabled = true
     tenant_id          = var.tenant_id
   }
 
