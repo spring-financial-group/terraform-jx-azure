3299544cd5213c8160fe463d6b5e052e063972dd
GavinPJK <gavin.kirwan@mqube.com>
2025-08-12 14:52:04 +0100

    chore: deploy bitnami OCI cache pullthrough

--- cluster/terraform-jx-registry-acr/main.tf
+++ cluster/terraform-jx-registry-acr/main.tf
@@ -56,3 +56,12 @@ resource "azurerm_container_registry_cache_rule" "cache_rule" {
   source_repo           = "docker.io/*"
   credential_set_id     = "${azurerm_container_registry.acr[0].id}/credentialSets/dockerhub-cred"
 }
+
+resource "azurerm_container_registry_cache_rule" "cache_rule_bitnami" {
+  count                 = var.acr_enabled && var.external_registry_url == "" && var.use_existing_acr_name == null ? 1 : 0
+  name                  = "registry-1-docker-io"
+  container_registry_id = azurerm_container_registry.acr[0].id
+  target_repo           = "registry-1.docker-io/*"
+  source_repo           = "registry-1.docker.io/*"
+  credential_set_id     = "${azurerm_container_registry.acr[0].id}/credentialSets/dockerhub-cred"
+}
