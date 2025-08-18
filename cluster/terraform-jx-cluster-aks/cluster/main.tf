237632055e3abba16959ebc5a4add8637f2511d8
mentlak <mentlak@outlook.com>
2025-08-18 11:01:41 +0100

    fix: mlnode with correct changes

--- cluster/terraform-jx-cluster-aks/cluster/main.tf
+++ cluster/terraform-jx-cluster-aks/cluster/main.tf
@@ -81,7 +81,6 @@ resource "azurerm_kubernetes_cluster_node_pool" "mlnode" {
   kubernetes_cluster_id       = azurerm_kubernetes_cluster.aks.id
   vm_size                     = var.ml_node_size
   fips_enabled                = false
-  gpu_driver                  = "Install"
   vnet_subnet_id              = var.vnet_subnet_id
   node_count                  = var.use_spot_ml ? 0 : var.ml_node_count
   min_count                   = var.min_ml_node_count
