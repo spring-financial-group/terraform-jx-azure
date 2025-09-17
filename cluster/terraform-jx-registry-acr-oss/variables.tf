248d738c0f577d50188a0242c4acd37b58fad047
Matthew Mentlak <93769720+mentlak0@users.noreply.github.com>
2025-09-17 17:36:39 +0100

    fix: allow passing of vars to modules (#179)

--- cluster/terraform-jx-registry-acr-oss/variables.tf
+++ cluster/terraform-jx-registry-acr-oss/variables.tf
@@ -26,13 +26,11 @@ variable "sku" {
 variable "oss_acr_enabled" {
   description = "Flag to indicate whether to provision Open Source ACR"
   type        = bool
-  default     = false
 }
 
 variable "oss_acr_pull_enabled" {
   description = "Flag to indicate whether to provision OSS ACR pull role assignment"
   type        = bool
-  default     = false
 }
 
 
