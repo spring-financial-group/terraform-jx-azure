222bf67c38ddc216635c83768dcaf9aa81e3c3f8
Gavin Kirwan <kirwangp@tcd.ie>
2025-09-19 12:41:49 +0100

    feat: additional ACR cache rules (#176)
    feat: add acr cache rules

--- cluster/terraform-jx-registry-acr/main.tf
+++ cluster/terraform-jx-registry-acr/main.tf
@@ -72,3 +72,27 @@ resource "azurerm_container_registry_cache_rule" "cache_rule_quay" {
   target_repo           = "quay-io/*"
   source_repo           = "quay.io/*"
 }
+
+resource "azurerm_container_registry_cache_rule" "cache_rule_gcr" {
+  count                 = var.acr_enabled && var.external_registry_url == "" && var.use_existing_acr_name == null ? 1 : 0
+  name                  = "gcr-io"
+  container_registry_id = azurerm_container_registry.acr[0].id
+  target_repo           = "gcr-io/*"
+  source_repo           = "gcr.io/*"
+}
+
+resource "azurerm_container_registry_cache_rule" "cache_rule_ghcr" {
+  count                 = var.acr_enabled && var.external_registry_url == "" && var.use_existing_acr_name == null ? 1 : 0
+  name                  = "ghcr-io"
+  container_registry_id = azurerm_container_registry.acr[0].id
+  target_repo           = "ghcr-io/*"
+  source_repo           = "ghcr.io/*"
+}
+
+resource "azurerm_container_registry_cache_rule" "cache_rule_k8s" {
+  count                 = var.acr_enabled && var.external_registry_url == "" && var.use_existing_acr_name == null ? 1 : 0
+  name                  = "registry-k8s-io"
+  container_registry_id = azurerm_container_registry.acr[0].id
+  target_repo           = "registry-k8s-io/*"
+  source_repo           = "registry.k8s.io/*"
+}
