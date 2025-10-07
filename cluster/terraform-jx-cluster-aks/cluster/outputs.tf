0b5b8962f7a8ecadb772cc47c2c8e7ce1ec97cff
mentlak <mentlak@outlook.com>
2025-10-07 11:00:34 +0100

    fix: outputs

--- cluster/terraform-jx-cluster-aks/cluster/outputs.tf
+++ cluster/terraform-jx-cluster-aks/cluster/outputs.tf
@@ -2,16 +2,16 @@ output "fqdn" {
   value = azurerm_kubernetes_cluster.aks.fqdn
 }
 output "cluster_endpoint" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config.host
+  value = azurerm_kubernetes_cluster.aks.kube_admin_config.*.host
 }
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config.client_certificate
+  value = azurerm_kubernetes_cluster.aks.kube_admin_config.*.client_certificate
 }
 output "client_key" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config.client_key
+  value = azurerm_kubernetes_cluster.aks.kube_admin_config.*.client_key
 }
 output "ca_certificate" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config.cluster_ca_certificate
+  value = azurerm_kubernetes_cluster.aks.kube_admin_config.*.cluster_ca_certificate
 }
 output "kube_config_admin_raw" {
   value = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
