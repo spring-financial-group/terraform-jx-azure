108509ddfca479eb4b7524de77cf5edc741baefa
GavinPJK <gavin.kirwan@mqube.com>
2026-02-13 10:24:44 +0000

    chore: set enable_dev_acr_pull in auto vars

--- cluster/variables.tf
+++ cluster/variables.tf
@@ -509,7 +509,7 @@ variable "enable_mqube_tech_acr_readonly" {
 
 variable "enable_dev_acr_pull" {
   type        = bool
-  default     = true
+  default     = false
   description = "Flag to grant MQube Product Engineering Team ACR pull permissions"
 }
 
