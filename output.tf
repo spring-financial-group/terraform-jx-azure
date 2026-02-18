49ccbb7d01f2cb10c16a00799e5c9ab3b2def212
Archie Skeoch <jaa.skeoch@gmail.com>
2026-02-18 16:16:52 +0000

    feat: add cluster_outbound_ip_addresses dummy output (#224)

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "registry_name" {
 output "resource_group_name" {
   value = module.cluster.resource_group_name
 }
+
+output "cluster_outbound_ip_addresses" {
+  value = ["51.11.169.142"] // Todo: Hook this up to the actual output from the cluster module
+}
