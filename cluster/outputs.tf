dc6c6f9143f1caed7d163b4acf9f636e2ba24bde
mentlak <mentlak@outlook.com>
2026-06-04 11:40:32 +0100

    test: cluster enablement

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -16,3 +16,27 @@ output "cluster_outbound_ip_addresses" {
 output "cluster_subnet_id" {
   value = module.cluster.cluster_subnet_id
 }
+
+output "kubelet_client_id" {
+  value = module.cluster.kubelet_client_id
+}
+
+output "cluster_endpoint" {
+  value = module.cluster.cluster_endpoint
+}
+
+output "client_certificate" {
+  value = module.cluster.client_certificate
+}
+
+output "client_key" {
+  value = module.cluster.client_key
+}
+
+output "ca_certificate" {
+  value = module.cluster.ca_certificate
+}
+
+output "kube_config_admin_raw" {
+  value = module.cluster.kube_config_admin_raw
+}
